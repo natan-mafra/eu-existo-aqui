1bc4a0a005ed0f56684987645052de9ce051db7d
Natan Mafra <natanmafra77@gmail.com>
2025-07-10 00:16:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const words = [
+  { text: "transsexual", lang: "en" },
+  { text: "transexual", lang: "pt-br" },
+  { text: "transexual", lang: "es" },
+  { text: "transexuelle", lang: "fr" },
+  { text: "トランスセクシュアル", lang: "ja" },
+];
+
+let current = 0;
+const wordElement = document.getElementById("word");
+
+function updateWord() {
+  const word = words[current];
+  wordElement.textContent = word.text;
+  wordElement.href = `pages/${word.lang}.html`;
+  current = (current + 1) % words.length;
+}
+
+updateWord();
+setInterval(updateWord, 2000);
