482d3a15024c18ce58eee4874539bf29ea63a2c5
Natan Mafra <natanmafra77@gmail.com>
2025-07-11 11:29:38 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const languages = [
+    {"country":"Afeganistão", "language":"Pastó", "word":"ترنس جنس"},
+    {"country":"África do Sul", "language":"Zulu", "word":"i-transgender"},
+    {"country":"Alemanha", "language":"Alemão", "word":"Transgender"},
+    {"country":"Arábia Saudita", "language":"Árabe", "word":"متحول جنسيا"},
+    {"country":"Argentina", "language":"Espanhol", "word":"Transgénero"},
+    // (continua com os outros 95 países da lista fornecida)
+];
---
+++ script.js
@@ -0,0 +1,17 @@
+let index = 0;
+const wordDiv = document.getElementById("word");
+
+function showWord() {
+    const current = languages[index];
+    wordDiv.innerText = current.word;
+    wordDiv.dataset.country = current.country;
+    index = (index + 1) % languages.length;
+}
+
+function redirect() {
+    const country = wordDiv.dataset.country;
+    window.location.href = "pessoas.html?country=" + encodeURIComponent(country);
+}
+
+setInterval(showWord, 500);
+showWord();
