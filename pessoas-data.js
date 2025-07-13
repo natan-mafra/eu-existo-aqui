7c315cfb3e60c49105ab71ffb2adbdd697490e9a
Natan Mafra <natanmafra77@gmail.com>
2025-07-13 17:31:15 -0300

    Add files via upload

---
+++ pessoas-data.js
@@ -0,0 +1,254 @@
+const pessoasPorPais = {
+  "afeganistao": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "africadosul": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "alemanha": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "arabiasaudita": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "argentina": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "australia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "austria": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "bangladesh": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "belgica": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "bolivia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "brasil": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "canada": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "chile": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "china": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "colombia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "coreiadosul": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "costarica": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "cuba": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "dinamarca": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "egito": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "emiradosarabes": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "equador": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "espanha": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "estadosunidos": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "filipinas": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "finlandia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "franca": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "grecia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "guatemala": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "honduras": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "hungria": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "india": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "indonesia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "ira": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "iraque": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "irlanda": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "islandia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "italia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "japao": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "jordania": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "kuwait": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "libano": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "libia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "malasia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "marrocos": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "mexico": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "mocambique": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "nepal": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "nigeria": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "noruega": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "novazelandia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "paisesbaixos": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "paquistao": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "paraguai": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "peru": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "polonia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "portugal": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "qatar": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "quenia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "reinounido": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "republicadominicana": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "republicatcheca": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "romenia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "russia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "senegal": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "serraleoa": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "singapura": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "siria": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "somalia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "srilanka": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "suecia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "suica": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "tailandia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "tanzania": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "tunisia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "turquia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "ucrania": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "uganda": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "uruguai": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "uzbequistao": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "venezuela": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "vietna": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "zambia": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+  "zimbabue": [
+    { name: "Nome Exemplo", img: "https://via.placeholder.com/150" }
+  ],
+};
