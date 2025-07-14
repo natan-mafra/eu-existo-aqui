1d998e788433c7195f32324f4e33b7736c4dd7c3
Natan Mafra <natanmafra77@gmail.com>
2025-07-14 13:56:09 -0300

    Update pessoas-data.js

--- pessoas-data.js
+++ pessoas-data.js
@@ -1,26 +1,26 @@
 
 const pessoasPorPais = {
   "alemanha": [
-    { "name": "Nyke Slawik", "img": "images/nyke-slawik.jpg" },
-    { "name": "Tessa Ganserer", "img": "images/tessa-ganserer.jpg" }
+    { "name": "Nyke Slawik", "img": "images/nyke-slawik.webp" },
+    { "name": "Tessa Ganserer", "img": "images/tessa-ganserer.webp" }
   ],
   "argentina": [
-    { "name": "Flor de la V", "img": "images/flor-de-la-v.jpg" },
-    { "name": "Mariana Genesio Peña", "img": "images/mariana-genesio-pena.jpg" }
+    { "name": "Flor de la V", "img": "images/flor-de-la-v.webp" },
+    { "name": "Mariana Genesio Peña", "img": "images/mariana-genesio-pena.webp" }
   ],
   "brasil": [
-    { "name": "Erika Hilton", "img": "images/erika-hilton.jpg" },
-    { "name": "Linn da Quebrada", "img": "images/linn-da-quebrada.jpg" },
-    { "name": "Laerte Coutinho", "img": "images/laerte-coutinho.jpg"}
+    { "name": "Erika Hilton", "img": "images/erika-hilton.webp" },
+    { "name": "Linn da Quebrada", "img": "images/linn-da-quebrada.webp" },
+    { "name": "Laerte Coutinho", "img": "images/laerte-coutinho.webp"}
   ],
   "estadosunidos": [
-    { "name": "Laverne Cox", "img": "images/laverne-cox.jpg" },
-    { "name": "Elliot Page", "img": "images/elliot-page.jpg" },
-    { "name": "Hunter Schafer", "img": "images/hunter-schafer.jpg" }
+    { "name": "Laverne Cox", "img": "images/laverne-cox.webp" },
+    { "name": "Elliot Page", "img": "images/elliot-page.webp" },
+    { "name": "Hunter Schafer", "img": "images/hunter-schafer.webp" }
   ],
   "reinounido": [
-      { "name": "Munroe Bergdorf", "img": "images/munroe-bergdorf.jpg"},
-      { "name": "Shon Faye", "img": "images/shon-faye.jpg"}
+      { "name": "Munroe Bergdorf", "img": "images/munroe-bergdorf.webp"},
+      { "name": "Shon Faye", "img": "images/shon-faye.webp"}
   ],
   "afeganistao": [], "africadosul": [], "arabiasaudita": [], "australia": [], "austria": [], "bangladesh": [], "belgica": [], "bolivia": [], "canada": [], "chile": [], "china": [], "colombia": [], "coreiadosul": [], "costarica": [], "cuba": [], "dinamarca": [], "egito": [], "emiradosarabes": [], "equador": [], "espanha": [], "filipinas": [], "finlandia": [], "franca": [], "grecia": [], "guatemala": [], "honduras": [], "hungria": [], "india": [], "indonesia": [], "ira": [], "iraque": [], "irlanda": [], "islandia": [], "italia": [], "japao": [], "jordania": [], "kuwait": [], "libano": [], "libia": [], "malasia": [], "marrocos": [], "mexico": [], "mocambique": [], "nepal": [], "nigeria": [], "noruega": [], "novazelandia": [], "paisesbaixos": [], "paquistao": [], "paraguai": [], "peru": [], "polonia": [], "portugal": [], "qatar": [], "quenia": [], "republicadominicana": [], "republicatcheca": [], "romenia": [], "russia": [], "senegal": [], "serraleoa": [], "singapura": [], "siria": [], "somalia": [], "srilanka": [], "suecia": [], "suica": [], "tailandia": [], "tanzania": [], "tunisia": [], "turquia": [], "ucrania": [], "uganda": [], "uruguai": [], "uzbequistao": [], "venezuela": [], "vietna": [], "zambia": [], "zimbabue": []
 };
